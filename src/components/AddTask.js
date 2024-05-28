74ca7c62e36baf6029a5b4c79b1bf9e3f103dc6f
Hassan Foadi <hasan.foadi@outlook.com>
2024-05-28 20:25:41 +1000

    Interaction with Back-end. Making changes on the permanent.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,20 +1,51 @@
-const AddTask = () => {
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("please Add a Task");
+      return;
+    }
+    onAdd({ text, day, reminder });
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
   return (
-    <form className="add-form">
+    <form className="add-form " onSubmit={onSubmit}>
       <div className="form-control">
         <lable>Task</lable>
-        <input type="text" placeholder="Add Task" />
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <lable>Day and Time</lable>
-        <input type="text" placeholder="Add Day and Time" />
+        <input
+          type="text"
+          placeholder="Add Day and Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
-      <div className="form-control">
+      <div className="form-control form-control-check">
         <lable>Set Reminder</lable>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
 
-      <input type="submit" value="Save Task"></input>
+      <input type="submit" value="Save Task" className="btn btn-block"></input>
     </form>
   );
 };
